124072b9e5bfba232fab5a4978a847c94d84a356
yarigg405 <south-nort@ya.ru>
2025-10-11 00:51:46 +0300

    ships pack

---
+++ src/space-traders/Assets/Pack/Scripts/RotateAround.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// It is used to rotate an object on Z axis
+/// </summary>
+
+public class RotateAround : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(0, 0, Time.deltaTime * 300);
+    }
+}
