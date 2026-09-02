e88e43afbfcec96adf73f0d1152cadc2d866b25b
yarigg405 <south-nort@ya.ru>
2026-09-02 20:05:36 +0300

    fix editor order

--- src/space-traders/Assets/Code/UI/Screens/ItemInfo/ItemInfoTabView.cs
+++ src/space-traders/Assets/Code/UI/Screens/ItemInfo/ItemInfoTabView.cs
@@ -6,14 +6,13 @@ namespace Assets.Code.UI.Screens.ItemInfo
 {
     public sealed class ItemInfoTabView : MonoBehaviour
     {
-        [field: SerializeField] public string TabId { get; private set; }
         [field: SerializeField] public Button TabButton { get; private set; }
-
-
         [SerializeField] private GameObject _selectedIndicator;
 
         [Space]
         [SerializeField] private GameObject _page;
+        [field: SerializeField] public string TabId { get; private set; }
+
 
         public void SetActive(bool active)
         {
